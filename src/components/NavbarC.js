03e4022cfc208286c88ae6feacc0c142e4ecb4d9
teraclitos <tefans12@gmail.com>
2023-02-27 19:54:37 -0300

    corrijo navbar

--- src/components/NavbarC.js
+++ src/components/NavbarC.js
@@ -1,36 +1,43 @@
 import React from "react";
-
 import { Container, Nav, Navbar, Button } from "react-bootstrap";
+import "../styles/all.css";
 
 const NavbarC = () => {
   return (
-    <>
-      <Navbar
-        className="sticky-top mb-5"
-        collapseOnSelect
-        expand="lg"
-        bg="dark"
-        variant="dark"
-      >
-        <Container className="">
-          <Navbar.Brand href="/">Gori</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="d-flex ms-auto align-items-center">
-              <Nav.Link className="me-lg-5" href="/">
-                Inicio
-              </Nav.Link>
-              <Nav.Link className="me-lg-5" href="/propiedades">
-                Propiedades
-              </Nav.Link>
-              <Nav.Link href="#pricing">
-                <Button> Contáctame</Button>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
+    <Navbar
+      className=" mb-5 nav-bar "
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+      sticky="top"
+    >
+      <Container className="">
+        <Navbar.Brand href="/">
+          <img
+            className="img-logo"
+            src="https://img.freepik.com/vector-premium/logo-casa_23-2147507198.jpg?w=740"
+            alt=""
+          />
+        </Navbar.Brand>
+
+        <Navbar.Toggle className="" aria-controls="responsive-navbar-nav" />
+
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="d-flex ms-auto align-items-center">
+            <Nav.Link className="me-lg-5" href="/">
+              Inicio
+            </Nav.Link>
+            <Nav.Link className="me-lg-5" href="/propiedades">
+              Propiedades
+            </Nav.Link>
+            <Nav.Link href="#pricing">
+              <Button> Contáctame</Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
