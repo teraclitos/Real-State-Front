e03d8beb15dd5d39def676cf017732dda6e6390f
teraclitos <tefans12@gmail.com>
2023-03-23 11:01:09 -0300

    mejoro el titulo del header detail

--- src/components/PropertyDetailHeader.js
+++ src/components/PropertyDetailHeader.js
@@ -7,29 +7,15 @@ const PropertyDetailHeader = ({ dataDetails }) => {
   return (
     <Container className="pt-5 pb-3 container-header-detail">
       <p className="grey text-center title grey-header-detail text-header-detail">
-        Propiedad <span className="light-grey"> {dataDetails.name}</span>,
-        ubicada en
-        <span className="light-grey"> {dataDetails.location}</span>, con un
-        precio de
+        <span className="gross-letter normal-letter-header">Propiedad</span>{" "}
+        <span className="light-grey"> {dataDetails.name},</span>
+        <span className="gross-letter normal-letter-header"> ubicada en</span>
+        <span className="light-grey"> {dataDetails.location}, </span>
+        <span className="gross-letter normal-letter-header">
+          con un precio de
+        </span>
         <span className="light-grey"> US$ {dataDetails.price}</span>
       </p>
-      {/* <Row className=" pb-5 pt-5 pb-md-0 header-detail-container light-grey ">
-        <Col className="d-flex justify-content-center col-md-4 col-12  ">
-          <h5 className="py-md-0 my-2 letter-size-header ">
-            {dataDetails.name}
-          </h5>
-        </Col>
-        <Col className=" col-md-4 d-flex justify-content-center col-12   ">
-          <h5 className="py-md-0 my-2 letter-size-header ">
-            US$ {dataDetails.price}
-          </h5>
-        </Col>
-        <Col className=" col-md-4 d-flex justify-content-center  col-12   ">
-          <h5 className="py-md-0 my-2 letter-size-header">
-            {dataDetails.location}
-          </h5>
-        </Col>
-      </Row> */}
     </Container>
   );
 };
