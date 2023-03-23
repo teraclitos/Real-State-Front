e7288a5581f42e1ed06621c31c02f3da629b803e
teraclitos <tefans12@gmail.com>
2023-03-23 10:31:07 -0300

    agrego logo de la pagina y h1 2

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -22,9 +22,9 @@ const Hero = ({
       <div className="hero-container ">
         <div className="slogan-container "></div>
         <div className="slogan-cube-container d-flex align-items-center justify-content-center ">
-          <h2 className="slogan px-3 px-sm-0 mb-5 ">
+          <h1 className="slogan px-3 px-sm-0 mb-5 ">
             Tenemos el inmueble que buscas, no pierdas tiempo
-          </h2>
+          </h1>
           <Link
             onClick={() => {
               setLoader(true);
