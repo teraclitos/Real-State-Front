9605673f04ac14157cb80e2776ea388f4fd9d33b
teraclitos <tefans12@gmail.com>
2023-06-06 17:15:58 -0300

    mejoro el contactame

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,35 +15,37 @@ const Contact = () => {
     }, 1200);
   }, []);
   return (
-    <Container className="py-5">
-      <h2
-        style={{ opacity: titleContact, transition: "1s" }}
-        className="  title fs-1 title-contact text-center  light-grey"
-      >
-        Contáctame
-      </h2>
+    <Container className="">
+      <div className="d-flex flex-column justify-content-center align-items-center pt-5">
+        <h2
+          style={{ opacity: titleContact, transition: "1s" }}
+          className="  title title-contact text-center  light-grey "
+        >
+          Contáctame
+        </h2>
 
-      <div className="d-flex flex-sm-row flex-column  justify-content-center align-items-center contact-container">
-        <Link
-          target={"_blank"}
-          to="https://www.facebook.com/marketplace/profile/100021849893616"
-          className="link"
-        >
-          <FontAwesomeIcon
-            className="light-grey me-sm-5 mb-5 mb-sm-0 mt-5 mt-sm-0 icon-contact icon-contact-left"
-            icon={faFacebook}
-          />
-        </Link>
-        <Link
-          target={"_blank"}
-          to="https://wa.me/543816293327?text=Hola,%20c%C3%B3mo%20est%C3%A1s?%F0%9F%98%83%0AUsted%20se%20encuentra%20en%20contacto%20con%20Gonzalo%20de%20la%20Vega,%20asesor%20comercial%20inmobiliario%20%F0%9F%8F%A1%0AEstoy%20a%20su%20servicio,%20en%20qu%C3%A9%20puedo%20ayudarlo"
-          className="link"
-        >
-          <FontAwesomeIcon
-            className="light-grey  me-3 icon-contact icon-contact-right"
-            icon={faWhatsapp}
-          />
-        </Link>
+        <div className="d-flex flex-sm-row flex-column  justify-content-center align-items-center contact-container">
+          <Link
+            target={"_blank"}
+            to="https://www.facebook.com/marketplace/profile/100021849893616"
+            className="link"
+          >
+            <FontAwesomeIcon
+              className="light-grey me-sm-5 mb-5 mb-sm-0 mt-5 mt-sm-0 icon-contact icon-contact-left"
+              icon={faFacebook}
+            />
+          </Link>
+          <Link
+            target={"_blank"}
+            to="https://wa.me/543816293327?text=Hola,%20c%C3%B3mo%20est%C3%A1s?%F0%9F%98%83%0AUsted%20se%20encuentra%20en%20contacto%20con%20Gonzalo%20de%20la%20Vega,%20asesor%20comercial%20inmobiliario%20%F0%9F%8F%A1%0AEstoy%20a%20su%20servicio,%20en%20qu%C3%A9%20puedo%20ayudarlo"
+            className="link"
+          >
+            <FontAwesomeIcon
+              className="light-grey  me-3 icon-contact icon-contact-right"
+              icon={faWhatsapp}
+            />
+          </Link>
+        </div>
       </div>
     </Container>
   );
