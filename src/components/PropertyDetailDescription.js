170193be840dd2d91e1046d17dd9f17fa9a8856b
Francisco <tefans12@gmail.com>
2024-10-15 16:22:38 -0300

    corrijo descripcion

--- src/components/PropertyDetailDescription.js
+++ src/components/PropertyDetailDescription.js
@@ -6,7 +6,7 @@ const PropertyDetailDescription = ({ dataDetails }) => {
   return (
     <Container className="container my-5 py-2 px-3 description-container">
       <h5 className="title black mb-2 font-size-detail-title">Descripción</h5>
-      <p className="d-inline-block ps-3 font-size-detail description">
+      <p className="d-inline-block ps-3 font-size-detail ">
         {dataDetails.description}
       </p>
     </Container>
